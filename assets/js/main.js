5f2bf0bb3d7accf49eab3407d4163b1eb9aa64da
Lucyloules <lucia.lagos@gmail.com>
2018-11-15 21:31:51 -0300

    otras vistas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,23 @@
 $(document).ready(function () { 
   $('.owl-carousel').owlCarousel();
-  
-  $('.heart').click(function () {
+
+/*   $('.heart').click(function () {
     this.css('color', 'red');
   });
+
+  $('#hide').click(function () {
+    //$('#show').before('<p>Prueba</p>');
+    $('h1').hide();
+  })
+  $('#show').click(function () {
+    //$('#show').before('<p>Prueba</p>');
+    $('h1').show();
+  }) */
+$('.button-up').click(function(){
+  $('body, html').animated({
+    scrollTop: '0px'
+  });
+});
 
 });
   
